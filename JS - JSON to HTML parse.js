e11541f1cc266fe3a12bd30aaf9efaaf2f1d68f9
corlord <coreylord@gmail.com>
2021-11-12 19:36:25 -0700

    Initial JSON 2 HTML JS file for Alex to evaluate

---
+++ JS - JSON to HTML parse.js
@@ -0,0 +1,36 @@
+let jOb = JSON.parse(document.body.textContent);
+const root = document.body;
+
+function eval_json(j_obj, root_node){
+    if (Array.isArray(j_obj)){
+        for (var j in j_obj){
+            eval_json(j_obj[j], root_node);
+        }
+    } else {
+        build_nodes(j_obj, root_node);
+    };
+};
+
+function build_nodes(j_obj, root_node){
+    for(var k in j_obj){
+        element = document.createElement("div");
+        root_node.appendChild(element);
+        element_span1 = document.createElement("span");
+        element_span1.innerText = k;
+        element_span1.className = k;
+        element_span1.style.padding = "25px";
+        element.appendChild(element_span1);
+        if (Array.isArray(j_obj[k])){
+            eval_json(j_obj[k], element);
+        } else if (typeof j_obj[k] === "object") {
+            build_nodes(j_obj[k], element);
+
+        } else {
+        element_span2 = document.createElement("span");
+        element_span2.innerText = j_obj[k];
+        element.appendChild(element_span2);
+        };
+    };
+};
+
+eval_json(jOb, root);
