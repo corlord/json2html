f8c032030d3a8e98070ce5e30a3d1b37a770246d
corlord <coreylord@gmail.com>
2021-11-13 12:10:35 -0700

    Added key to className for parent div, comments

--- JS - JSON to HTML parse.js
+++ JS - JSON to HTML parse.js
@@ -1,6 +1,9 @@
 let jOb = JSON.parse(document.body.textContent);
 const root = document.body;
 
+// Evaluates immediate json objects to determine if it is an array
+// True: Iterates through the array creates sub node and retests array items
+// False: Sends to build_nodes to create elements based on key/values.
 function eval_json(j_obj, root_node){
     if (Array.isArray(j_obj)){    
         for (var j in j_obj){
@@ -15,10 +18,14 @@ function eval_json(j_obj, root_node){
     };
 };
 
+// Iterates through key/value pairs to create span nodes to hold each separately under a unique parent, uses key value as classname in elements to help xpath parsing. 
+// If arrays or additional json objects are encountered, calls the appropriate function to handle.
 function build_nodes(j_obj, root_node){
     for(var k in j_obj){
-        element = document.createElement("div");
+        var element = document.createElement("div");
         root_node.appendChild(element);
+        element.className = k;
+        
         element_span1 = document.createElement("span");
         element_span1.innerText = k;
         element_span1.className = k;
